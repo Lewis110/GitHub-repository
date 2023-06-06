54973471a04d4ff3ad9ddf2a859cb3134b1a46da
Will <69062137+0xEquinox@users.noreply.github.com>
2023-06-06 08:35:45 -0700

    Update main.py
    Fizzbuzz

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("hello")
+
+for i in range(100):
+  if i % 15 == 0:
+    print("Fizzbuzz")
+  elif i % 5 == 0:
+    print("Buzz")
+  elif i % 3 == 0:
+    print("Fizz")
+  else:
+    print(i)
